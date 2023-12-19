59a3c46ea4907da5c8d25070563c312c2cdefb2a
Bamwesigye-Calvin-Kiiza <bamwesigyecalvinkiiza@gmail.com>
2023-12-19 10:37:13 +0300

    adding comment and comments view added

--- Demo_app/urls.py
+++ Demo_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path 
 from .views import home
 from . import views
+from .views import AddComment,commentview
 app_name = 'Demo_app'
 
 urlpatterns = [
     path('home/',views.home,name ='home'),
     path('',views.landingpage,name ='landingpage'),
     path('roadmap/',views.roadmap,name ='roadmap'),
+    path('addcomment/',AddComment.as_view(),name='addcomment'),
+    path('comments/',commentview.as_view(),name='comment'),
 ]
--- Demo_app/views.py
+++ Demo_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
-
+from django.views.generic.edit import CreateView,DeleteView,UpdateView,FormView
+from django.views.generic.list import ListView
+from .models import *
+from django.urls import reverse_lazy
 # Create your views here.
 
 def home (request):
@@ -10,3 +13,16 @@ def landingpage (request):
 
 def roadmap(request):
     return render(request,'Demo_app/roadmap.html',{})
+
+class AddComment(CreateView):
+    model = Comment
+    fields = ('title', 'message')
+    template_name = 'Demo_app/addcomment.html'
+    success_url = reverse_lazy('Demo_app:home')
+
+class commentview(ListView):
+    model = Comment
+    template_name = 'Demo_app/comments.html'
+    context_object_name = 'comment_list'
+    
+    
